7dff4c31c56a32e0d4eec417cc64b42538470e66
Gbarabe Tordi Joseph <tordigbarabe100@gmail.com>
2022-09-08 06:57:42 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - print a text
+ *
+ * return: Always 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
