d88b5dc7ee04531f762ffce71f2f27f468c48334
Gbarabe Tordi Joseph <tordigbarabe100@gmail.com>
2022-09-09 13:49:20 -0700

    task 6

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int d;
+
+	for (d = '0'; <= '9'; d++)
+		putchar(\n');
+
+	return (0);
+}
