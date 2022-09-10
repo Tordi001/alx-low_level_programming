468c1ac6c9c04ff891f1e787ff1bd435258f07c0
Gbarabe Tordi Joseph <tordigbarabe100@gmail.com>
2022-09-09 21:24:00 -0700

    task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - printf("programming is like building a multilingual puzzle")
+ * main - prints "programming is like building a multilingual puzzle"
  * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
 	put("\"programming is like building a multilingual puzzle");
-
 	return (0);
 }
