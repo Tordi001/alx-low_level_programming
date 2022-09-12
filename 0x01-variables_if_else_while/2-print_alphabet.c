30c7011bd0467740a7dd3649a010e7233528e694
Gbarabe Tordi Joseph <tordigbarabe100@gmail.com>
2022-09-12 01:47:02 -0700

    task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,13 +6,11 @@
  */
 int main(void)
 {
-	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
+	char c;
 
-	int i;
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
 
-	for (i = 0; i < 26; i++)
-	{
-		putchar(alphabet[i]);
-	}
+	putchar('\n');
 	return (0);
 }
