dcf98063b7317a6c270bdf139f0f4b6e2c38edae
Gbarabe Tordi Joseph <tordigbarabe100@gmail.com>
2022-09-12 01:52:28 -0700

    task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,12 +6,19 @@
  */
 int main(void)
 {
-	char ch;
-	char CH;
-	for (ch = 'a'; ch <= 'z'; ch++)
-		putchar(ch);
-	for (CH = 'A'; CH <= 'Z'; CH++)
-		putchar(CH);
-			putchar("\n");
-			return (0);
+	char c;
+
+	for (c = 'a'; c <= 'z' c++)
+	{
+		putchar(c);
+	}
+
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+	
+	return (0);
 }
