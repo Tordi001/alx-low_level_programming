1fae76014a6a29bed38a19ab3ddcd4510edb720f
Gbarabe Tordi Joseph <tordigbarabe100@gmail.com>
2022-09-13 10:38:13 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include <main.h>
+
+/**
+ * print_alphabet_x10 - prints 10 times
+ * REturn: Always (0)
+ */
+
+void print_alphabet_x10(void)
+{
+	int alphabet_count = 0;
+
+	while (alphabet_count < 10)
+	{
+		char (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		alphabet_count++;
+		_putchar('\n');
+
+	}
+}
