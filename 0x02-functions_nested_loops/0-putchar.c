c4d516ae8a9aa458fadeac13b06a2632bfed672f
Gbarabe Tordi Joseph <tordigbarabe100@gmail.com>
2022-09-13 08:27:58 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Retun: Always 0
+ */
+{
+	_putchar('H');
+	_putchar('O');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+
+	return (0);
+}
