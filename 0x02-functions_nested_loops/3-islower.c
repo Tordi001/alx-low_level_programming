1fc3ea10c21f21429f220fd5254d13dab52d870f
Gbarabe Tordi Joseph <tordigbarabe100@gmail.com>
2022-09-13 22:36:26 -0700

    task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _slower - checks for lowercase character
+ * _islower - checks for lowercase character
  * @c: The character to be checked
  *
  * Return: 1 for lowercase cgaracter or 0 for anything else
