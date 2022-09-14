e8511d65e8d1acefa5239d8e2ba038074bbf138f
Gbarabe Tordi Joseph <tordigbarabe100@gmail.com>
2022-09-14 01:56:45 -0700

    _putchar.c

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,14 @@
+#include "holberton"
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * on error, -1 is required, and errno is set appropriately.
+ */
+int _putcharrrrr(char c)
+{
+	return (write(1, &c, 1));
+}
