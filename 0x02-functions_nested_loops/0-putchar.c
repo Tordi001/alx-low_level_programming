b9901345aac6408c9266203195381d99c2429c03
Gbarabe Tordi Joseph <tordigbarabe100@gmail.com>
2022-09-14 02:07:51 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,20 @@
 #include "holberton.h"
 
 /**
- * main - Prints "HOlberton" followed by a new line.
+ * main - Prints _putchar followed by a new line.
  *
- * Retun: Always 0
+ * Retun: Always 0 (Success)
  */
 int main(void)
 {
-	_putchar('H');
-	_putchar('O');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
 	_putchar('t');
-	_putchar('o');
-	_putchar('n');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 
 	return (0);
