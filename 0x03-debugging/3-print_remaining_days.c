377a50b1c498193dbb7d4801978f906e8bcd5943
Gbarabe Tordi Joseph <tordigbarabe100@gmail.com>
2022-09-14 21:43:02 -0700

    task 3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -24,6 +24,10 @@ void print_remainnnnnnnnnnnnnning_days(int month, int day, int year)
 	{
 		if (month == 2 && day == 60)
 		{
+			printf("invalid date: %02d/%02d/%04d\n",month, day - 31, year);
+		}
+		else
+		{
 			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 365 -day);
 		}
