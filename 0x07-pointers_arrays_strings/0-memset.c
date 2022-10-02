6b9250434ce014cb04bf3fc5e0783f5ab3af99fd
Gbarabe Tordi Joseph <tordigbarabe100@gmail.com>
2022-10-02 00:11:35 -0700

    task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,18 +1,32 @@
 #include "main.h"
 
 /**
+
  * _memset - fills memory with a constant byte,
+
  * @s: memory area.
+
  * @b: constant byte.
+
  * @n: bytes filled.
- * Return: the power to dest.
+
+ * Return: the pointer to dest.
+
  */
-char *_memcpy(char *dest, char *src, unsigned int n);
+
+char *_memset(char *s, char b, unsigned int n)
+
 {
+
 	unsigned int i;
 
-	for (i =0; i < n; i++)
-		*(s + i) = b;
+
+
+	for (i = 0; i < n; i++)
+
+		*(s + i) =  b;
+
+
 
 	return (s);
-}
+
