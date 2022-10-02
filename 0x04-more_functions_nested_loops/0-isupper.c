bcc9d0a06e7db25b405cf482b0c3c82fcadce252
Gbarabe Tordi Joseph <tordigbarabe100@gmail.com>
2022-10-02 00:46:30 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
   * _isupper - Check if a letter is upper
@@ -6,9 +6,9 @@
   *
   * Return: 1 for upper letter or 0 for any else
   */
-int _isupper(int x)
+int _isupper(int c)
 {
-	if (x >= 65 && x <= 90)
+	if (c >= 65 && c <= 90)
 	{
 		return (1);
 	}
